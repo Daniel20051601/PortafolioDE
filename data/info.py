993f8f62d02bc5d62f219920ded2eb2c15967de8
Danielvi <danielvilopez@gmail.com>
2026-05-12 20:55:21 -0400

    ✨Agrega información sobre proyectos y tecnologías al archivo info.py

---
+++ data/info.py
@@ -0,0 +1,30 @@
+PROJECTS = [
+    {
+        "url_image" : "assets/images/content/cinerecommender2.png",
+        "title" : "🎬 Sistema de Recomendación de Películas",
+        "description" : """Aplicación desarrollada en Python capaz de recomendar películas similares mediante 
+                        técnicas de filtrado y procesamiento de datos para generar recomendaciones personalizadas.""",
+        "url_link" : "https://github.com/Daniel20051601/CineRecomender.git",
+        "list_technologies" : ['Python', 'PosgreSQL','PostgreSQL' ,'REST APIs', 'Pandas', 'Streamlit']
+    },
+    {
+        "url_image" : "assets/images/content/infaliblerealestate.png",
+        "title" : "🏠 Plataforma Web Inmobiliaria — Infalible Real Estate",
+        "description" : """Plataforma web inmobiliaria desarrollada con .NET y Blazor Server para la gestión de propiedades, citas, usuarios y foros, incorporando funcionalidades de filtrado, autenticación y administración de contenido.""",
+        "url_link" : "https://infaliblerealestate.com/",
+        "list_technologies" : ['C#', 'Blazor Server', '.NET', 'PostgreSQL', 'REST APIs', 'Supabase']
+    },
+    {
+        "url_image" : "assets/images/content/infaliblerealestateapp2.png",
+        "title" : "📱Aplicacion Movil Inmobiliaria - Infalible Real Estate",
+        "description" : """Aplicación móvil nativa para Android enfocada en el sector inmobiliario. 
+            Permite a los usuarios explorar, filtrar y guardar propiedades, con integración 
+            de contacto directo vía WhatsApp. Desarrollada completamente en Kotlin bajo principios de 
+            Clean Architecture y el patrón MVI, utilizando Jetpack Compose para una UI moderna 
+            y WorkManager para sincronización de datos en segundo plano.""",
+        "url_link" : "https://github.com/Daniel20051601/InfalibleRealEstateApp.git",
+        "list_technologies" : ['Kotlin', 'Jetpack Compose', 'REST APIs', 'PostgreSQL']
+    },
+]
+
+TECHNOLOGIES = ["Python", "SQL", "Apache Airflow", "Pandas", "PostgreSQL", "Supabase", "Docker", "C#",'.NET', "Kotlin", "Blazor Server", "Streamlit", "REST APIs", "Git", "GitHub"]
