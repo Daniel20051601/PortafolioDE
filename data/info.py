0891cf2b49db9469be2091a9bd83fc5ea68b7949
Danielvi <danielvilopez@gmail.com>
2026-06-05 19:52:53 -0400

    ♻️ Actualiza info.py para agregar un nuevo parametro a los proyectos y agrega nueva constante para almacenar la información inicial

--- data/info.py
+++ data/info.py
@@ -1,16 +1,24 @@
+ABOUTME = """
+            Estudiante de :red[Ingeniería en Sistemas] apasionado por el área de :red[**Ingeniería de Datos**], 
+            la automatización y el aprendizaje continuo, con experiencia en desarrollo de software y en la creación 
+            de soluciones orientadas al procesamiento e integración eficiente de datos.
+            """
+
 PROJECTS = [
     {
         "url_image" : "assets/images/content/cinerecommender2.png",
         "title" : "🎬 Sistema de Recomendación de Películas",
         "description" : """Aplicación desarrollada en Python capaz de recomendar películas similares mediante 
                         técnicas de filtrado y procesamiento de datos para generar recomendaciones personalizadas.""",
-        "url_link" : "https://github.com/Daniel20051601/CineRecomender.git",
+        "url_code" : "https://github.com/Daniel20051601/CineRecomender.git",
+        "url_link" : "https://cinerecommender.streamlit.app/",
         "list_technologies" : ['Python', 'PosgreSQL', 'REST APIs', 'Apache Airflow', 'Pandas', 'Streamlit']
     },
     {
         "url_image" : "assets/images/content/infaliblerealestate.png",
         "title" : "🏠 Plataforma Web Inmobiliaria — Infalible Real Estate",
         "description" : """Plataforma web inmobiliaria desarrollada con .NET y Blazor Server para la gestión de propiedades, citas, usuarios y foros, incorporando funcionalidades de filtrado, autenticación y administración de contenido.""",
+        "url_code" : "",
         "url_link" : "https://infaliblerealestate.com/",
         "list_technologies" : ['C#', 'Blazor Server', '.NET', 'PostgreSQL', 'REST APIs', 'Supabase']
     },
@@ -22,7 +30,8 @@ PROJECTS = [
             de contacto directo vía WhatsApp. Desarrollada completamente en Kotlin bajo principios de 
             Clean Architecture y el patrón MVI, utilizando Jetpack Compose para una UI moderna 
             y WorkManager para sincronización de datos en segundo plano.""",
-        "url_link" : "https://github.com/Daniel20051601/InfalibleRealEstateApp.git",
+        "url_code" : "https://github.com/Daniel20051601/InfalibleRealEstateApp.git",
+        "url_link" : "",
         "list_technologies" : ['Kotlin', 'Jetpack Compose', 'REST APIs', 'PostgreSQL']
     },
 ]
